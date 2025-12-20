3838cc808beac1ca55b7b4ec10f99f4247da87cb
Esteban Loaiza <eloaiza@devinci.cl>
2025-12-19 22:54:00 -0300

    correccion para coolify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,28 @@
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// import tailwindcss from '@tailwindcss/vite';
+
+// // https://astro.build/config
+// import vercel from '@astrojs/vercel';
+
+// export default defineConfig({
+//   output: 'server',
+//   adapter: vercel(),
+//   vite: {
+//     plugins: [tailwindcss()]
+//   }
+// });
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node'; // <--- Cambiado
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: node({
+    mode: 'standalone', // <--- Esto le dice a Astro que cree un servidor autónomo
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
